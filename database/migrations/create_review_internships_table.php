95bc0fb5a9255f173344eefe8efdca228b09dff4
Arne Ameye <arne.ameye@outlook.com>
2019-10-02 21:47:39 +0200

    Moved migrations file for review_companies_table into the right folder (mistake correction) and changing 2 migration files' classnames to correspond with the filename itself

--- database/migrations/create_review_internships_table.php
+++ database/migrations/create_review_internships_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInternshipsReviewTable extends Migration
+class CreateReviewInternshipsTable extends Migration
 {
     /**
      * Run the migrations.
