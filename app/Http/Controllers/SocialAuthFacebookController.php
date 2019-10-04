057e05d2146ccb51e29b4d6fafd0b2ecbb143414
Arne Ameye <arne.ameye@outlook.com>
2019-10-04 18:54:46 +0200

    Add use of a service in the FB Auth controller to handle the fb login process

--- app/Http/Controllers/SocialAuthFacebookController.php
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Socialite;
+use App\Services\SocialFacebookAccountService;
 
 class SocialAuthFacebookController extends Controller
 {
