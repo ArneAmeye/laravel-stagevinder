8348eb5dc9998b058bedc3f9c4c4b1f4948f9e18
haeirene <haegeman.irene@hotmail.com>
2019-10-04 17:18:04 +0200

    Create John Doe dummy data in StudentsSeeder

--- database/seeds/StudentsSeeder.php
+++ database/seeds/StudentsSeeder.php
@@ -9,6 +9,14 @@ class StudentsSeeder extends Seeder
      */
     public function run()
     {
+        $student = new \App\Student();
+        $student->fistname = 'John';
+        $student->lastname = 'Doe';
+        $student->email = 'johndoe@stagevinder.be';
+        $student->password = Hash::make('password');
+        $student->field_study = 'Webdevelopment';
+        $student->school = 'Thomas More Highschool';
+        $student->bio = 'Lorem Ipsum';
         factory(\App\Students::class, 50)->create();
     }
 }
