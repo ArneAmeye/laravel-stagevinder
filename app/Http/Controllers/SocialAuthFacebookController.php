e6acc1c1dfdb6f127968bec5ce311752b1bb018d
Arne Ameye <arne.ameye@outlook.com>
2019-10-04 16:57:01 +0200

    adding Socialite, FacebookAuthController and Facebook App credentials -WIP for FB auth

---
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SocialAuthFacebookController extends Controller
+{
+
+    /**
+     * Create a redirect method to facebook api.
+     *
+     * @return void
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+    /**
+     * Return a callback method from facebook api.
+     *
+     * @return callback URL from facebook
+     */
+    public function handleProviderCallback(SocialFacebookAccountService $service)
+    {
+        $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
+        auth()->login($user);
+        return redirect()->to('/home');
+    }
+}
--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Other Service Providers...
+         */
+        Laravel\Socialite\SocialiteServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Other aliases…
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
