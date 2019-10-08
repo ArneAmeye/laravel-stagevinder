542f3258d377b978fae2a994a798ef550b68ce65
haeirene <haegeman.irene@hotmail.com>
2019-10-08 21:32:10 +0200

    Optimize breadcrumb for index page - Breadcrumbs are optimized to automatically display the pages name. - This makes it easy for further reuse.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,28 @@
 @section('title')
     Home
 @endsection
+@section('stylesheet')
+	{{ asset('css/pages/student.css') }}
+@endsection
 @section('content')
-    
+    @component('components/breadcrumb')
+		@slot('breadcrumb')
+		@endslot
+	@endcomponent
+    <div class="preview__container">
+        <section class="preview__container">
+            <div class="preview__inner">
+                <div class="preview__header">
+                    <h5 class="preview__title">
+                        Name internship
+                    </h5>
+			    </div>
+                <div class="preview__body">
+                    <p class="preview__text">
+                        Proffesion
+                    </p>
+                </div>
+		    </div>
+		</section>
+    </div>
 @endsection
