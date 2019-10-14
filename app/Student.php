3a20c1d1db894d1cfa3c522bf3fed7a57ac7e7ad
haeirene <haegeman.irene@hotmail.com>
2019-10-14 10:35:18 +0200

    Add casts - Used to verify a email

--- app/Student.php
+++ app/Student.php
@@ -6,17 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-	protected $table = 'students';
+    protected $table = 'students';
 
-    protected $fillable = ['user_id', 'facebook_user_id', 'firstname', 'lastname', 'email', "password"];
+    protected $fillable = ['user_id', 'facebook_user_id', 'firstname', 'lastname', 'email', 'password'];
 
     protected $hidden = [
         'password', 'remember_token',
     ];
-    
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
