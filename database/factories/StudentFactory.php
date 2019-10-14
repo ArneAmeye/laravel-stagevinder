39c9ffad61a7341dc0a98f2d47bab2d61741fbfe
haeirene <haegeman.irene@hotmail.com>
2019-10-14 22:58:43 +0200

    Modify columns of tables in database - Add email and password in student table (as it is already done in companies but not in students) - Modify factory of student to add faker email and password - Modify seeder to show once again the default email and password, which is now used to include in the student table - Modify records of table companies to accept default value of '' - This makes it easier to register as not all data can immediately filled.

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -10,6 +10,8 @@ $factory->define(\App\Student::class, function (Faker $faker) {
     return [
         'firstname' => $faker->firstName(),
         'lastname' => $faker->lastName,
+        'email' => $faker->email,
+        'password' => Hash::make($faker->password),
         'birth_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
         'profile_picture' => $faker->imageUrl($width = 200, $height = 200),
         'background_picture' => $faker->imageUrl($width = 1180, $height = 213),
