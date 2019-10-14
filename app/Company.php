d8113019841775e2b10a89fe3121a85f3f8efc46
haeirene <haegeman.irene@hotmail.com>
2019-10-14 10:39:36 +0200

    Add functions to Company - Functions are used for authentication

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-	//
+    use Notifiable;
+
+    protected $guard = 'companies';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
