1f7f0972758a2056279502a56b27ffa0d86bf5b4
Arne Ameye <arne.ameye@outlook.com>
2019-10-17 12:37:38 +0200

    remove old getcompanydetails function because we are now using an Ajax based in it's own AjaxController

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -42,40 +42,6 @@ class CompanyController extends Controller
         return view('companies/show', $data);
     }
 
-    public function getCompanyData()
-    {
-        //Start new Guzzle client -> Used for fetching API data
-        $client = new Client();
-
-        //parameters from frontend needed for Foursquare API request
-        $location = 'Brussels, BE'; //replace with user input from frontend!
-        $companyName = 'panos'; //replace with user input from frontend!
-
-        //start up API GET request for a company near the given location
-        $result = $client->request('GET', 'https://api.foursquare.com/v2/venues/search', [
-            'query' => [
-                'client_id' => env('FOURSQUARE_CLIENT_ID'),
-                'client_secret' => env('FOURSQUARE_SECRET_ID'),
-                'v' => '20191009',
-                'near' => $location,
-                'query' => $companyName,
-                'limit' => 1,
-            ],
-        ]);
-        if ($result->getStatusCode() == 200) {
-            //Response from API is succesful, proceed with data!
-            $data['body'] = (array) $result->getBody();
-
-            return view('companies/add', $data);
-        } else {
-            //Response from API has ERROR, show error output!
-            $data['errorCode'] = (string) $result->getStatusCode();
-            $data['ErrorPhrase'] = (string) $result->getReasonPhrase();
-
-            return view('companies/add', $data);
-        }
-    }
-
     public function update($id, Request $request)
     {
         $company = \App\Company::where('id', $id)->first();
