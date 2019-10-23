68972e60da9b2b98ff0c5fc5264fd577634fa4d9
haeirene <haegeman.irene@hotmail.com>
2019-10-23 08:29:11 +0200

    Optimize search functionality - Make all models searchable. - Add basic consistent layout to search

--- app/Internship.php
+++ app/Internship.php
@@ -3,7 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Internship extends Model
 {
+    use Searchable;
 }
--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,13 +1,11 @@
-<div class="search__container">
-    <div>
-        <form action="/search" method="GET" class="search__field">
-			{{ csrf_field() }}
-			<input type="text" name="" placeholder="Search" class="search__field--input">
-			<Button class="search__field--btn">
-				<i class="fas fa-search" aria-hidden="true"></i>
-			</Button>
-		</form>
-    </div>
+<div class="search__field__container">
+    <form action="/search" method="GET" class="search__field">
+		{{ csrf_field() }}
+		<input type="text" name="" placeholder="Search" class="search__field--input">
+		<Button class="search__field--btn">
+			<i class="fas fa-search" aria-hidden="true"></i>
+		</Button>
+	</form>
 </div>
 <div class="search__results__container">
     <ul>
