ecdfc061e64192df931c3e3fe258950da8e2d2a1
haeirene <haegeman.irene@hotmail.com>
2019-10-25 13:37:47 +0200

    Bugfix creating internships - Wrong column name was used. Instead of 'sector', it was 'field_sector'

--- app/Http/Controllers/InternshipController.php
+++ app/Http/Controllers/InternshipController.php
@@ -3,21 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Hash;
-use App\Rules\Space;
 use Auth;
 use Session;
 
-class InternshipController extends Controller{
-
-    public function index(){
+class InternshipController extends Controller
+{
+    public function index()
+    {
         $data['internships'] = \App\Internship::get();
-        return view("internships/index", $data);
+
+        return view('internships/index', $data);
     }
 
-    public function show($internship){
+    public function show($internship)
+    {
         $data['internship'] = \App\Internship::find($internship)->where('id', $internship)->first();
 
         $data['company'] = \App\Company::where('id', $data['internship']->company_id)->first();
@@ -25,7 +25,7 @@ class InternshipController extends Controller{
         $data['current'] = auth()->user()->id;
 
         if (!empty($_GET['edit'])) {
-            $id = session()->get("user")->id;
+            $id = session()->get('user')->id;
             if ($id != $internship) {
                 return redirect("/internships/$internship");
             }
@@ -37,33 +37,34 @@ class InternshipController extends Controller{
         return view('internships/show', $data);
     }
 
-    public function create($company, Request $request){
-        $user = session()->get("user");
+    public function create($company, Request $request)
+    {
+        $user = session()->get('user');
 
         $internship = new \App\Internship();
         $internship->title = $request->input('title');
-        $internship->sector = $request->input('sector');
+        $internship->field_sector = $request->input('sector');
         $internship->description = $request->input('description');
         $internship->requirements = $request->input('requirements');
-        if(!empty($request->input('background_picture'))){
+        if (!empty($request->input('background_picture'))) {
             $internship->background_picture = $request->input('background_picture');
-        }else{
-            $internship->background_picture = "default.jpg";
+        } else {
+            $internship->background_picture = 'default.jpg';
         }
-        
+
         $internship->company_id = $company;
         $internship->is_available = 1;
 
         $internship->save();
 
-        return redirect("/internships/$internship->id")->with('success', "Internship has been added!");
+        return redirect("/internships/$internship->id")->with('success', 'Internship has been added!');
     }
 
-    public function update($id, Request $request){
-    
+    public function update($id, Request $request)
+    {
         $internship = \App\Internship::where('id', $id)->first();
 
-        $user = session()->get("user");
+        $user = session()->get('user');
         if ($user->id != $internship->company_id) {
             return redirect("/internships/$internship");
         }
@@ -86,8 +87,5 @@ class InternshipController extends Controller{
 
             return redirect("/internships/$id")->with('success', 'Internship details have been updated!');
         }
-
-
     }
-
 }
