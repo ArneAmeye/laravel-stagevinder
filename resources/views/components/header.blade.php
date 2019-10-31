7f087c7de382e1753dcf9daba35a6dea52916ef7
haeirene <haegeman.irene@hotmail.com>
2019-10-31 18:43:14 +0100

    Bugfix redirect to logout - In the href of the logout link was a route declined, this can also be used by just using /logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,10 @@
 @php
 	$headerOfUser = [
 		["name" => "Settings", "href" => "#", "icon" => "fa-cog"],
-		["name" => "Companies", "href" => "@if (Session::get('user')->type == 'student')/students/{{ Session::get('user')->id }}@elseif (Session::get('user')->type == 'company')/companies/{{ Session::get('user')->id }}@endif", "icon" => "fa-user"],
+		["name" => "Companies", "href" => `@if (Session::get('user')->type == 'student')/students/{{ Session::get('user')->id }}@elseif (Session::get('user')->type == 'company')/companies/{{ Session::get('user')->id }}@endif`, "icon" => "fa-user"],
 		["name" => "My Messages", "href" => "#", "icon" => "fa-envelope"],
 		["name" => "Lock Screen", "href" => "#", "icon" => "fa-lock"],
-		["name" => "Logout", "href" => "{{ route('user.logout') }}", "icon" => "fa-sign-out-alt"]
+		["name" => "Logout", "href" => "/logout", "icon" => "fa-sign-out-alt"]
 	];
 @endphp
 <header class="header__container">
@@ -30,43 +30,43 @@
 			</form>
 		</ul>
 		@if(Auth::check())
-		<ul class="header__options__items">
-			<li class="header__options__item">
-				<a href="#" class="header__options__link">
-					<i class="fas fa-bell header__options__icon" aria-hidden="true"></i>
-					<span class="header__options__badge header__options__badge--pink"></span>
-				</a>
-			</li>
-			<li class="header__options__item">
-				<a href="#" class="header__options__link">
-					<i class="fas fa-comments header__options__icon" aria-hidden="true"></i>
-					<span class="header__options__badge header__options__badge--green"></span>
-				</a>
-			</li>
-			<li class="header__options__item">
-				<a href="#" class="header__options__link">
-					<img src="/images/students/profile_picture/{{ Session::get('user')->profile_picture }}" class="header__options__profilePic">
-					<p class="header__options__name">
-						@if (Session::get('user')->type == 'student')
-							{{ Session::get('user')->firstname }} {{ Session::get('user')->lastname }}
-						@elseif (Session::get('user')->type == 'company')
-							{{ Session::get('user')->name }}
-						@endif
-					</p>
-					<i class="fas fa-angle-down header__options__more" aria-hidden="true"></i>
-				</a>
-				<ul class="options__more__items">
-					@foreach($headerOfUser as $item)
-						<li class="options__more__item">
-							<a href="{{$item['href']}}" class="options__more__link">
-								<i class="fa {{$item['icon']}} options__more__icon" aria-hidden="true"></i>
-								{{$item['name']}}
-							</a>
-						</li>
-					@endforeach
-				</ul>
-				@endif
-			</li>
-		</ul>
+			<ul class="header__options__items">
+				<li class="header__options__item">
+					<a href="#" class="header__options__link">
+						<i class="fas fa-bell header__options__icon" aria-hidden="true"></i>
+						<span class="header__options__badge header__options__badge--pink"></span>
+					</a>
+				</li>
+				<li class="header__options__item">
+					<a href="#" class="header__options__link">
+						<i class="fas fa-comments header__options__icon" aria-hidden="true"></i>
+						<span class="header__options__badge header__options__badge--green"></span>
+					</a>
+				</li>
+				<li class="header__options__item">
+					<a href="#" class="header__options__link">
+						<img src="/images/students/profile_picture/{{ Session::get('user')->profile_picture }}" class="header__options__profilePic">
+						<p class="header__options__name">
+							@if (Session::get('user')->type == 'student')
+								{{ Session::get('user')->firstname }} {{ Session::get('user')->lastname }}
+							@elseif (Session::get('user')->type == 'company')
+								{{ Session::get('user')->name }}
+							@endif
+						</p>
+						<i class="fas fa-angle-down header__options__more" aria-hidden="true"></i>
+					</a>
+					<ul class="options__more__items">
+						@foreach($headerOfUser as $item)
+							<li class="options__more__item">
+								<a href="{{$item['href']}}" class="options__more__link">
+									<i class="fa {{$item['icon']}} options__more__icon" aria-hidden="true"></i>
+									{{$item['name']}}
+								</a>
+							</li>
+						@endforeach
+					</ul>
+				</li>
+			</ul>
+		@endif
 	</div>
 </header>
