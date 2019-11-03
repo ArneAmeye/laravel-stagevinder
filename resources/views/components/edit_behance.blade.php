5660a40fa62097bb2a57cba940a08c1a2163dddb
Arne Ameye <arne.ameye@outlook.com>
2019-11-03 13:51:47 +0100

    added placeholder text

--- resources/views/components/edit_behance.blade.php
+++ resources/views/components/edit_behance.blade.php
@@ -2,7 +2,7 @@
 	{{ csrf_field() }}
     {{ method_field('patch') }}
 
-	<input type="text" class="input" name="behance" placeholder="" value="{{ $behanceUrl }}">
+	<input type="text" class="input" name="behance" placeholder="Behance portfolio URL" value="{{ $behanceUrl }}">
 	<div class="button__center">
 		<button class="button button--big" name="update_behance">Save</button>
 		<a href="{{ url('students/') }}/{{ $id }}" class="button button--transparent">Cancel</a>
