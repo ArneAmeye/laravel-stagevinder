4f4d2a6bbaa3b1d3e95339d1f868e915c2fa6810
Arne Ameye <arne.ameye@outlook.com>
2019-11-03 14:14:33 +0100

    expanding updateBehance method with call to BehanceController to do first API call

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -116,10 +116,15 @@ class StudentController extends Controller
 
         $student->behance = $request->input('behance');
 
+        //Make the first API call and save the portfolio items (JSON response)
+        $student->behance_api_result = \App\Behance::getBehancePortfolio($request->input('behance'));
+        //Check if API call returns data, if not show error.
+        // ** TODO: redirect("/students/$id?edit=behance")->withErrors
+
         $student->save();
 
         return redirect("/students/$id")
-            ->with('success', 'Behance portfolio URL has been updated!');
+            ->with('success', 'Behance portfolio has been updated!');
     }
 
     private static function updateDetails($id, Request $request)
