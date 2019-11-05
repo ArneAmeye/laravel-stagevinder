12d140949212f75ca06a0ccbebc547a744a80029
haeirene <haegeman.irene@hotmail.com>
2019-11-05 20:49:51 +0100

    Add vue data and method

--- resources/js/vue/search.js
+++ resources/js/vue/search.js
@@ -13,3 +13,36 @@ Vue.component("get", {
 		</div>
 	</a>`
 });
+
+var app = new Vue({
+    el: "body",
+    data: {
+        result: [],
+        //loading: false,
+        error: false,
+        query: ""
+    },
+    mounted: function() {
+        this.search();
+    },
+    methods: {
+        search: function() {
+            // Clear the error message.
+            this.error = "";
+            // Empty the products array so we can fill it with the new products.
+            this.products = [];
+
+            // Making a get request to our API and passing the query to it.
+            this.$http.get("/api/search?q=" + this.query).then(response => {
+                // If there was an error set the error message, if not fill the products array.
+                response.body.error
+                    ? (this.error = response.body.error)
+                    : (this.products = response.body);
+                // The request is finished, change the loading to false again.
+                this.loading = false;
+                // Clear the query.
+                this.query = "";
+            });
+        }
+    }
+});
