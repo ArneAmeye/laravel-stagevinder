1cf672d84c7adb61ac5daeb7ebabc8ce51d38fcd
haeirene <haegeman.irene@hotmail.com>
2019-12-05 20:06:33 +0100

    Bugfix when not authorized and you click on an internship, you need to be logged in.

--- resources/views/components/show_internships.blade.php
+++ resources/views/components/show_internships.blade.php
@@ -2,19 +2,23 @@
     <h3 class="preview__container__title">All available internships:</h3>
     @foreach($internships as $internship)
         @if($internship->is_available == 1)
-            <a href="{{ url('/internships/') }}/{{ $internship->id }}">
-                <div class="preview__inner">
-                    <img class="preview__image" src="{{asset('images/internships/profile_picture')}}/{{ $internship->profile_picture }}">
-                    <div class="preview__text">
-                        <p class="preview__text--internship">
-                            {{ $internship->title }}
-                        </p>
-                        <p class="preview__text--position">
-                            {{ $internship->description }}
-                        </p>
+            @if(Auth::check() and Session::has('user'))
+                <a href="{{ url('/internships/') }}/{{ $internship->id }}">
+            @else
+                <a href="/login">
+            @endif
+                    <div class="preview__inner">
+                        <img class="preview__image" src="{{asset('images/internships/profile_picture')}}/{{ $internship->profile_picture }}">
+                        <div class="preview__text">
+                            <p class="preview__text--internship">
+                                {{ $internship->title }}
+                            </p>
+                            <p class="preview__text--position">
+                                {{ $internship->description }}
+                            </p>
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
         @endif
     @endforeach
 </div>
