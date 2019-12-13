e932eb965e895131ab51593426b99f349e75285e
Arne Ameye <arne.ameye@outlook.com>
2019-12-13 14:14:30 +0100

    fix position fixed on register & login page cutting out top of the form on smaller screens

--- public/js/remove_button.js
+++ public/js/remove_button.js
@@ -113,7 +113,7 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! d:\Bureaublad\Thomas More\Sem 5\Webtech Advanced Back\PHP2\laravel-app\resources\js\remove_button.js */"./resources/js/remove_button.js");
+module.exports = __webpack_require__(/*! D:\Bureaublad\Thomas More\Sem 5\Webtech Advanced Back\PHP2\laravel-app\resources\js\remove_button.js */"./resources/js/remove_button.js");
 
 
 /***/ })
