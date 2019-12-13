2a002028c2833130c97326eb7ab101560b33c4d4
Arne Ameye <arne.ameye@outlook.com>
2019-12-13 16:00:17 +0100

    solving merge conflicts

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -134,11 +134,7 @@ function getCompanyDetails() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 module.exports = __webpack_require__(/*! C:\Users\haege\Dropbox\thomas_more\3IMD_A\advanced_webtech_back\laravel\projecten\laravel-stagevinder\resources\js\ajax.js */"./resources/js/ajax.js");
-=======
-module.exports = __webpack_require__(/*! D:\Bureaublad\Thomas More\Sem 5\Webtech Advanced Back\PHP2\laravel-app\resources\js\ajax.js */"./resources/js/ajax.js");
->>>>>>> a57067f6f8f1ffe8e06d0452b1363a9a3369fb3b
 
 
 /***/ })
